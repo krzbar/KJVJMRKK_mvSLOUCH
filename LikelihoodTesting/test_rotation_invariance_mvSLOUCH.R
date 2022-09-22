a3c12c9daa9ea5dc9d0b473742d7f50a5bf7e909
krzbar <bartoszekkj@gmail.com>
2022-09-22 21:25:41 +0200

    correct preamble

--- LikelihoodTesting/test_rotation_invariance_mvSLOUCH.R
+++ LikelihoodTesting/test_rotation_invariance_mvSLOUCH.R
@@ -1,5 +1,5 @@
 ## This file accompanies the manuscript: 
-## Bartoszek, Fuentes Gonzalez, Mitov, Pienaar, Piwczyński, Puchałka, Spalik and Voje " Rotation Invariance in non–Brownian motion Phylogenetic Comparative Methods: A comment on Adams and Collyer (2018)"
+## Bartoszek, Fuentes Gonzalez, Mitov, Pienaar, Piwczynski, Puchalka, Spalik and Voje " Rotation Invariance in non–Brownian motion Phylogenetic Comparative Methods: A comment on Adams and Collyer (2018)"
 
 ## This software comes AS IS in the hope that it will be useful WITHOUT ANY WARRANTY, 
 ## NOT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
