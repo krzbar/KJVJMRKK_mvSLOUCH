dbee5941445fc79bec5732b50303ef1710c46f40
krzbar <bartoszekkj@gmail.com>
2022-09-22 13:28:11 +0200

    Added preamble

--- Ferula_analyses/simulation_study/modelbm_var.R
+++ Ferula_analyses/simulation_study/modelbm_var.R
@@ -1,3 +1,13 @@
+## This file accompanies the manuscript: 
+## Bartoszek, Fuentes Gonzalez, Mitov, Pienaar, Piwczyński, Puchałka, Spalik and Voje " Model Selection Performance in Phylogenetic Comparative Methods under multivariate Ornstein–Uhlenbeck Models of Trait Evolution"
+
+## This software comes AS IS in the hope that it will be useful WITHOUT ANY WARRANTY, 
+## NOT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+## Please understand that there may still be bugs and errors. Use it at your own risk. 
+## We take no responsibility for any errors or omissions in this code or for any misfortune 
+## that may befall you or others as a result of its use. Please send comments and report 
+## bugs to Krzysztof Bartoszek at krzbar@protonmail.ch .
+
 load("Ferula_Data.RData")
 seed2 <- 1
 set.seed(seed2)
