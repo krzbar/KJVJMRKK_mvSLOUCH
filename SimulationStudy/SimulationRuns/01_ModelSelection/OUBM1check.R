2edf603d40f2ed1ca90f78a7593f36cefc588fd8
krzbar <bartoszekkj@gmail.com>
2023-03-10 21:45:03 +0100

    With median and quartiles

--- SimulationStudy/SimulationRuns/01_ModelSelection/OUBM1check.R
+++ SimulationStudy/SimulationRuns/01_ModelSelection/OUBM1check.R
@@ -1,85 +1,47 @@
-## This file accompanies the manuscript: 
-## Bartoszek, Fuentes Gonzalez, Mitov, Pienaar, Piwczynski, Puchalka, Spalik and Voje " Rotation Invariance in non–Brownian motion Phylogenetic Comparative Methods: A comment on Adams and Collyer (2018)"
-## It generates the values of Tab. 3 and should be run in the directory
-## KJVJMRKK_mvSLOUCH/SimulationStudy/SimulationRuns/01_ModelSelection/
-## containg the SimulationReestimation_SetupID_01_N_*.RData files
+[1] "Setup BM"
+[1] "Model simulated under:"
+$evolmodel
+[1] "bm"
 
+[1] "Model being compared with:"
+$evolmodel
+[1] "mvslouch"
 
-## This software comes AS IS in the hope that it will be useful WITHOUT ANY WARRANTY, 
-## NOT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-## Please understand that there may still be bugs and errors. Use it at your own risk. 
-## We take no responsibility for any errors or omissions in this code or for any misfortune 
-## that may befall you or others as a result of its use. Please send comments and report 
-## bugs to Krzysztof Bartoszek at krzbar@protonmail.ch .
+$Atype
+[1] "SingleValueDiagonal"
 
+$diagA
+[1] "Positive"
 
-vn<-c(32,64,128,256,512,1024,2048)
-
-lalpha<-vector("list",length(vn))
-for (r in 1:length(vn)){
-    n<-vn[r]
-    load(paste0("SimulationReestimation_SetupID_01_N_",n,".RData"))
-    valpha<-rep(NA,length(lres))
-    for (i in 1:length(lres)){
-	BMAICc<-NA
-	bestOUBM1AICc<-Inf
-	jvalmvslouch<-NA
-	for (j in 1:length(lres[[i]]$mvsl_estim_res$model.setups)){
-	    if (lres[[i]]$mvsl_estim_res$model.setups[[j]]$evolmodel=="bm"){
-		BMAICc<-lres[[i]]$mvsl_estim_res$testedModels[[j]]$aic.c
-	    }else{
-		if ((lres[[i]]$mvsl_estim_res$model.setups[[j]]$evolmodel=="mvslouch")&&(lres[[i]]$mvsl_estim_res$model.setups[[j]]$Atype=="SingleValueDiagonal")){		
-		    tmpAICc<-lres[[i]]$mvsl_estim_res$testedModels[[j]]$aic.c
-		    if ((!is.null(tmpAICc)) &&(!is.na(tmpAICc)) && (tmpAICc<bestOUBM1AICc)){
-			bestOUBM1AICc<-tmpAICc;jvalmvslouch<-j
-		    }
-		}
-	    }	    
-	}
-	if ((!is.null(BMAICc)) &&(!is.na(BMAICc)) && (!is.null(bestOUBM1AICc)) &&(!is.na(bestOUBM1AICc)) && (!is.na(bestOUBM1AICc)) && (bestOUBM1AICc<BMAICc)){
-	    if (lres[[i]]$mvsl_estim_res$testedModels[[jvalmvslouch]]$result$MaxLikFound[1]!="Same as final found"){
-		valpha[i]<-lres[[i]]$mvsl_estim_res$testedModels[[jvalmvslouch]]$result$MaxLikFound$ParamsInModel$A[1,1]
-	    }else{
-		valpha[i]<-lres[[i]]$mvsl_estim_res$testedModels[[jvalmvslouch]]$result$FinalFound$ParamsInModel$A[1,1]
-	    }	    
-	}
-    }
-    lalpha[[r]]<-valpha
-}
-
-lvn<-length(vn)
-print("average t_0.5")
-to_print<-""
-for(i in 1:lvn){to_print<-paste0(to_print,round(mean(log(2)/(2*lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
-print("variance t_0.5")
-to_print<-""
-for(i in 1:lvn){to_print<-paste0(to_print,round(var(log(2)/(2*lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
-print("% t_0.5 < 0")
-to_print<-""
-cutoffval<-0;for(i in 1:lvn){to_print<-paste0(to_print,round(100*sum(log(2)/(2*lalpha[[i]])<cutoffval,na.rm=TRUE)/sum(!is.na(lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
-print("% t_0.5 > 0.5")
-to_print<-""
-cutoffval<-0.5;for(i in 1:lvn){to_print<-paste0(to_print,round(100*sum(log(2)/(2*lalpha[[i]])>cutoffval,na.rm=TRUE)/sum(!is.na(lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
-print("% t_0.5 > 1")
-to_print<-""
-cutoffval<-1;for(i in 1:lvn){to_print<-paste0(to_print,round(100*sum(log(2)/(2*lalpha[[i]])>cutoffval,na.rm=TRUE)/sum(!is.na(lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
-print("% t_0.5 > 2")
-to_print<-""
-cutoffval<-2;for(i in 1:lvn){to_print<-paste0(to_print,round(100*sum(log(2)/(2*lalpha[[i]])>cutoffval,na.rm=TRUE)/sum(!is.na(lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
-print("% t_0.5 > 3")
-to_print<-""
-cutoffval<-3;for(i in 1:lvn){to_print<-paste0(to_print,round(100*sum(log(2)/(2*lalpha[[i]])>cutoffval,na.rm=TRUE)/sum(!is.na(lalpha[[i]]),na.rm=TRUE),3),", ")}
-cat(paste0(to_print,"\n"))
-print("==================================================")
+[1] "average t_0.5"
+41.713, 1.08, 79.472, 5.273, 32.234, 14.936, 10.368, 
+[1] "=================================================="
+[1] "variance t_0.5"
+86583.633, 4.027, 195483.039, 183.675, 14518.482, 3552.124, 230.866, 
+[1] "=================================================="
+[1] "median t_0.5"
+0.264, 0.433, 0.644, 0.855, 1.604, 2.349, 3.826, 
+[1] "=================================================="
+[1] "lower quartile t_0.5"
+0.194, 0.264, 0.445, 0.69, 1.047, 1.688, 2.273, 
+[1] "=================================================="
+[1] "upper quartile t_0.5"
+0.447, 0.629, 1.022, 1.791, 3.446, 3.821, 9.681, 
+[1] "=================================================="
+[1] "% t_0.5 < 0"
+0, 0, 0, 0, 0, 0, 0, 
+[1] "=================================================="
+[1] "% t_0.5 > 0.5"
+18.868, 37.838, 66.667, 89.189, 96.429, 100, 100, 
+[1] "=================================================="
+[1] "% t_0.5 > 1"
+13.208, 16.216, 25, 45.946, 78.571, 92.857, 100, 
+[1] "=================================================="
+[1] "% t_0.5 > 2"
+11.321, 13.514, 13.889, 21.622, 35.714, 64.286, 75.758, 
+[1] "=================================================="
+[1] "% t_0.5 > 3"
+9.434, 8.108, 13.889, 18.919, 28.571, 35.714, 57.576, 
+[1] "=================================================="
+[1] "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+[1] "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
